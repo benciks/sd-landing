15f8417c48235a3d03b9aa38f09dd5dad8361c90
Simon Bencik <bencikdesign@gmail.com>
2021-04-01 14:42:11 +0200

    Stole a navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,207 @@
+<template>
+  <header :class="{ sticky }">
+    <div class="header-container">
+      <NuxtLink to="/">
+        <Logo />
+      </NuxtLink>
+      <nav>
+        <div class="links">
+          <NuxtLink v-for="(link, index) in links" :key="index" :to="link.to" class="bold">
+            {{ link.name }}
+          </NuxtLink>
+        </div>
+        <div class="actions">
+          <div class="menu-toggle" @click="openMobileMenu">
+            <MenuIcon />
+          </div>
+        </div>
+      </nav>
+    </div>
+    <div class="mobile-nav" :class="{ active: mobileMenuOpen }">
+      <nav>
+        <div class="close" @click="closeMobileMenu">
+          <XIcon />
+        </div>
+        <div class="links">
+          <NuxtLink
+            v-for="(link, index) in links"
+            :key="index"
+            :to="link.to"
+            @click.native="handleNavigation"
+          >
+            {{ link.name }}
+          </NuxtLink>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script lang="js">
+import Vue from 'vue'
+import { XIcon, MenuIcon } from 'vue-feather-icons'
+
+export default Vue.extend({
+  components: {
+    XIcon,
+    MenuIcon
+  },
+  data () {
+    return {
+      links: [
+        { name: 'Domov', to: '/' },
+        { name: 'Podujatia', to: '/events' },
+        { name: 'Školy', to: '/schools' },
+        { name: 'Články', to: '/articles' }
+      ],
+      wScroll: 0,
+      sticky: false,
+      mobileMenuOpen: false
+    }
+  },
+  beforeMount () {
+    window.addEventListener('scroll', this.handleScroll)
+    window.addEventListener('resize', this.handleResize)
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.handleScroll)
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    openMobileMenu () {
+      this.mobileMenuOpen = true
+    },
+    closeMobileMenu () {
+      this.mobileMenuOpen = false
+    },
+    handleResize () {
+      this.mobileMenuOpen = false
+    },
+    handleNavigation () {
+      this.closeMobileMenu()
+    },
+    handleScroll () {
+      this.wScroll = window.scrollY
+      if (this.wScroll > 50) {
+        this.sticky = true
+      } else {
+        this.sticky = false
+      }
+    }
+  }
+})
+</script>
+
+<style scoped lang="sass">
+header
+  z-index: 100
+  position: fixed
+  width: 100%
+  transition: all 300ms ease
+  background: none
+  &.sticky
+    background: white
+    padding-top: 0px
+    box-shadow: $small-shadow
+  .header-container
+    width: 100%
+    padding: $xxl 0
+    display: flex
+    align-items: center
+    justify-content: space-between
+
+    nav
+      display: flex
+      align-items: center
+      .actions
+        display: flex
+        align-items: center
+      a + a
+        margin-left: $xxl
+  nav
+    margin: 0
+    a
+      @extend %copy-m
+      color: $ui3
+      text-decoration: none
+      font-weight: normal
+      font-size: 16px
+      line-height: 16px
+      &.nuxt-link-exact-active
+        font-weight: bold
+        color: $ui1
+      &.home.nuxt-link-exact-active.nuxt-link-active
+        font-weight: bold
+  .mobile-nav
+    position: fixed
+    background: white
+    width: 100%
+    height: 100%
+    top: 0
+    left: 100%
+    padding: 100px 40px 60px 40px
+    transition: 200ms ease-in-out
+
+    a
+      font-size: 24px
+    &.active
+      left: 0%
+    .close
+      position: absolute
+      top: 60px
+      right: 24px
+      width: 48px
+      height: 48px
+      display: flex
+      align-items: center
+      justify-content: center
+      z-index: 10
+
+      svg
+        color: $ui3
+    nav
+      display: flex
+      height: 100%
+      flex-direction: column
+      justify-content: space-between
+      a
+        display: block
+        padding: $l
+
+@media screen and (max-width: 700px)
+  header
+    .header-container
+      padding: $xxl $xl
+      nav
+        .links, .actions .login
+          display: none
+        a + a
+          margin-left: $s
+        a
+          padding: 10px
+        .menu-toggle
+          margin-left: $s
+
+          svg
+            color: $ui1
+
+@media screen and (max-width: 1030px)
+  header
+    .header-container
+      padding: $xxl $xl
+
+@media screen and (min-width: 700px)
+  header
+    display: flex
+    justify-content: center
+    padding-top: 16px
+    .menu-toggle
+      display: none
+    .header-container
+      max-width: 1000px
+      nav
+        .actions
+          margin-left: $xl
+          a + a
+            margin-left: $l
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,5 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title order-1">
-        sd-landing
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <div />
 </template>
 
 <script>
@@ -32,6 +7,4 @@ export default {}
 </script>
 
 <style lang="sass">
-.title
-  color: $ui4
 </style>
