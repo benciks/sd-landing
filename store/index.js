b7a7671f1e961023fdffaada445021ebfd847d3c
Simon Bencik <bencikdesign@gmail.com>
2021-04-21 22:38:02 +0200

    Fixed the logout after reload issue

--- store/index.js
+++ store/index.js
@@ -7,3 +7,22 @@ export const getters = {
     return state.auth.user
   }
 }
+
+export const actions = {
+  async nuxtServerInit ({ commit }, { req }) {
+    let auth = null
+    if (req.headers.cookie) {
+      // cookie found
+      try {
+        // check data user login with cookie
+        const { data } = await this.$axios.post('/users/me')
+        // server return the data is cookie valid loggedIn is true
+        auth = data // set the data auth
+      } catch (err) {
+        // No valid cookie found
+        auth = null
+      }
+    }
+    commit('SET_AUTH', auth) // set state auth
+  }
+}
